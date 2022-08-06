70958cc4dc68d2d46ff2751959e938e143759b3d
Daniel Verdu <iamadeveloper.es@gmail.com>
2022-08-06 14:31:38 +0200

    fix mixin ripple-effect

--- src/mixin/rippleEffect.js
+++ src/mixin/rippleEffect.js
@@ -5,7 +5,7 @@ export default {
             const y = ev.clientY - ev.target.offsetTop;
 
             const ripples = document.createElement('span');
-            ripples.setAttribute('class', 'rippleSpan')
+            ripples.setAttribute('class', 'ripple-span')
             ripples.style.left = x + 'px';
             ripples.style.top = y + 'px';
             this.$el.appendChild(ripples);
