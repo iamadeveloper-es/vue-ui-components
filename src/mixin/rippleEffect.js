9744a83be7237cdbbcb6ea85ed78f0d344134e9f
Daniel Verdu <iamadeveloper.es@gmail.com>
2022-08-06 14:21:43 +0200

    Mixin ripple-effect to apply in any action component

---
+++ src/mixin/rippleEffect.js
@@ -0,0 +1,19 @@
+export default {
+    methods: {
+        rippleEffect(ev){
+            const x = ev.clientX - ev.target.offsetLeft;
+            const y = ev.clientY - ev.target.offsetTop;
+
+            const ripples = document.createElement('span');
+            ripples.setAttribute('class', 'rippleSpan')
+            ripples.style.left = x + 'px';
+            ripples.style.top = y + 'px';
+            this.$el.appendChild(ripples);
+
+            setTimeout(() => {
+                ripples.remove()
+            }, 1000);
+
+        }
+    }
+}
