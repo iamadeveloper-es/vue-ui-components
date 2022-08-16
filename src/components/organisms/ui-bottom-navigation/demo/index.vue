5e16bda0b239f56bbb72da3b28db9b12dd06b4ba
Daniel Verdu <iamadeveloper.es@gmail.com>
2022-08-16 20:07:07 +0200

    Created bottom navigation component

---
+++ src/components/organisms/ui-bottom-navigation/demo/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="ui-bottom-navigation-demo">
+    <p>Fixed menu on the bottom of the screen, scroll up and down</p>
+    <ui-bottom-navigation
+    :links="bottomLinks"
+    ></ui-bottom-navigation>
+  </div>
+</template>
+
+<script>
+import UiBottomNavigation from '../../ui-bottom-navigation';
+export default {
+  name: 'ui-bottom-navigation-demo',
+  components: {
+    UiBottomNavigation
+  },
+  data(){
+    return {
+      bottomLinks: [
+        {
+            route: { name: 'Ui-button'},
+            label: 'Health',
+            iconModel: {
+                type: 'solid',
+                icon: 'heart',
+                class: 'mb-1'
+            },
+            class: 'ui-link--icon-top'
+        },
+        {
+            route: { name: 'Ui-bottom-navigation'},
+            label: 'Contacts',
+            iconModel: {
+                type: 'solid',
+                icon: 'user',
+                class: 'mb-1'
+            },
+            class: 'ui-link--icon-top'
+        },
+        {
+            route: { name: 'Ui-bottom-navigation'},
+            label: 'Posts',
+            iconModel: {
+                type: 'solid',
+                icon: 'comment-alt',
+                class: 'mb-1'
+            },
+            class: 'ui-link--icon-top'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../mixins/ui-bottom-navigation';
+
+$base: (
+  'menu-padding': 15px 20px,
+  'color': var(--white),
+  'elevation': 0px 4px 14px var(--dark)
+);
+
+$modifiers: (
+  'padding': 8px 10px
+);
+
+.ui-bottom-navigation{
+    @include bottomNavigation($base, $modifiers)
+}
+</style>
---
+++ src/components/organisms/ui-bottom-navigation/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="ui-bottom-navigation"
+  :class="{'ui-bottom-navigation--hide' : hideNav}"
+  >
+    <nav class="ui-bottom-navigation__nav">
+      <ul 
+      class="ui-bottom-navigation__ul flex flex--center flex--wrap">
+        <li 
+        class="ui-bottom-navigation__li"
+        v-for="(link, index) in links"
+        :key="index"
+        >
+          <ui-link
+          :route="link.route"
+          :label="link.label"
+          :iconModel="link.iconModel"
+          :classes="link.class"
+          ></ui-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+import UiLink from '../../molecules/ui-link';
+export default {
+  name: 'ui-bottom-navigation',
+  components: {
+    UiLink
+  },
+  props: {
+    links: {
+      type: Array,
+      // required: true
+    },
+    hideOnScroll: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data(){
+    return {
+      lastScroll: null,
+      hideNav: false
+    }
+  },
+  methods: {
+    handleScroll() {
+      const scrollTo = window.pageYOffset || document.documentElement.scrollTop;
+      if (scrollTo > this.lastScroll){
+        this.hideNav = false;
+      } else {
+          this.hideNav = true;
+      }
+      this.lastScroll = scrollTo <= 0 ? 0 : scrollTo;
+    },
+    initialScrollPos(){
+      this.lastScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    },
+    setScroll(){
+      this.initialScrollPos();
+      window.addEventListener("scroll", this.handleScroll);
+    },
+    destroyScroll(){
+      window.removeEventListener("scroll", this.handleScroll);
+    }
+  },
+  created() {
+    if(this.hideOnScroll){
+      this.setScroll();
+    }
+  },
+  destroyed() {
+    if(this.hideOnScroll){
+      this.destroyScroll();
+    }
+  }
+}
+</script>
+
+<style>
+</style>
