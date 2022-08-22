d7f939be0aabfc8e3d561514cace0eba34e61318
Daniel Verdu <iamadeveloper.es@gmail.com>
2022-08-22 20:30:45 +0200

    Created checkbox-group component

---
+++ src/components/cells/ui-checkbox-group/demo/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="checkbox-group-demo">
+    <ui-checkbox-group
+    :checkboxes="checkboxes"
+    @checkboxValues="setValues"
+    ></ui-checkbox-group>
+    <p>Selected values: {{selectedValues}}</p>
+  </div>
+</template>
+
+<script>
+import UiCheckboxGroup from '../../ui-checkbox-group'
+export default {
+    name: 'checkbox-group-demo',
+    components: {
+        UiCheckboxGroup
+    },
+    data(){
+        return {
+            checkboxes: [
+                {
+                    color: 'primary',
+                    status: true,
+                    val: 'Jhon',
+                    label: 'User Jhon'
+                },
+                {
+                    color: 'info',
+                    val: 'Richard',
+                    label: 'User Richard'
+                },
+                {
+                    color: 'danger',
+                    val: 'Jack',
+                    label: 'User Jack'
+                },
+                {
+                    color: 'success',
+                    val: 'Daniel',
+                    label: 'User Daniel'
+                },
+                {
+                    color: 'success',
+                    disabled: true,
+                    val: 'Paul',
+                    label: 'User Paul'
+                }
+            ],
+            selectedValues: []
+        }
+    },
+    methods:{
+        setValues(ev){
+            this.selectedValues = ev
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import '../mixins/ui-checkbox-group';
+
+$base: ();
+
+.ui-checkbox-group {
+    @include checkboxGroup($base);
+}
+</style>
---
+++ src/components/cells/ui-checkbox-group/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="ui-checkbox-group">
+    <ui-checkbox
+    v-for="(checkbox, index) in checkboxes"
+    :key="index"
+    :activeColor="checkbox.color"
+    :initialStatus="checkbox.status"
+    :disabled="checkbox.disabled"
+    :label="checkbox.label"
+    v-model="checkbox.val"
+    @change="getValue"
+    ></ui-checkbox>
+  </div>
+</template>
+
+<script>
+import UiCheckbox from '../../molecules/ui-checkbox'
+export default {
+    name: 'ui-checkbox-group',
+    components: {
+        UiCheckbox
+    },
+    props: {
+        checkboxes: {
+            type: Array,
+            required: true
+        }
+    },
+    data(){
+        return {
+            checkboxValues: []
+        }
+    },
+    methods: {
+        getValue(ev){
+            if (ev.checked && !this.checkboxValues.includes(ev.value)) {
+                this.checkboxValues.push(ev.value);
+            }
+            else{
+                this.checkboxValues.forEach((item, index) => {
+                    if(item === ev.value){
+                        this.checkboxValues.splice(index, 1);
+                    }
+                })
+            }
+            this.$emit('checkboxValues', this.checkboxValues);
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
