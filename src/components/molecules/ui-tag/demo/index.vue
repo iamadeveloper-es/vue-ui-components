bff9670622d7e4c6d7132c31c14ec906970f0aec
Daniel Verdu <iamadeveloper.es@gmail.com>
2022-08-25 08:28:13 +0200

    Tag demo filter list example

--- src/components/molecules/ui-tag/demo/index.vue
+++ src/components/molecules/ui-tag/demo/index.vue
@@ -46,7 +46,7 @@
     <div class="mt-3">
       <p class="mb-1">Tag list</p>
       <ui-tag
-      v-for="(tag, index) in chips"
+      v-for="(tag, index) in tags"
       :key="index"
       :cClass="'mr-1 mb-1'"
       :label="`${tag.label} ${index + 1}`"
@@ -55,19 +55,46 @@
       :close="true"
       ></ui-tag>
     </div>
+    <div class="mt-3">
+      <p>Tag in filter list</p>
+      <div class="tags mb-2">
+        <ui-tag
+        v-for="(tag, index) in filteredTags"
+        :key="index"
+        :cClass="'mr-1'"
+        :label="tag.label"
+        ></ui-tag>
+      </div>
+      <ui-input-field
+      label="Search post"
+      :floatingLabel="true"
+      v-model="inputValue"
+      ></ui-input-field>
+      <div class="posts mt-3">
+        <div class="post"
+        v-for="(post, index) in filteredPosts"
+        :key="index"
+        >
+          <h5>{{post.title}}</h5>
+          <p class="mt-1">{{post.text}}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import UiInputField from '../../ui-input-field'
 import UiTag from '../../ui-tag'
 export default {
     name: 'ui-tag-demo',
     components: {
+      UiInputField,
         UiTag
     },
     data() {
       return {
-        chips: [
+        tags: [
           {
             label: 'Tag',
             bgColor: 'primary',
@@ -90,7 +117,59 @@ export default {
         filterIcon: {
           icon: 'twitter',
           type: 'brands'
-        }
+        },
+        posts: [
+          {
+            title: 'What is BEM',
+            text: 'BEM is a front-end naming method for organizing and naming CSS classes. The Block, Element, Modifier methodology is a popular naming convention for class names in HTML and CSS. It helps to write clean CSS by following some simple rules.',
+            category: 'technology'
+          },
+          {
+            title: 'Maximilien Robespierre',
+            text: 'Maximilien François Marie Isidore de Robespierre, better known as Maximilien Robespierre or Maximiliano Robespierre, was a French lawyer, writer, orator, and politician nicknamed "the Incorruptible".',
+            category: 'history'
+          },
+          {
+            title: 'VUE 3 Composition API',
+            text: 'Composition API is a set of APIs that allows us to author Vue components using imported functions instead of declaring options.',
+            category: 'technology'
+          },
+          {
+            title: 'Greece hotel',
+            text: 'Greece is a country in southeastern Europe with thousands of islands in the Aegean and Ionian seas. It was influential in ancient times and is often called the "cradle of Western civilization". Athens, its capital.',
+            category: 'travel'
+          }
+        ],
+        inputValue: '',
+        postsTags: [
+          {
+            label: 'technology',
+            close: true
+          },
+          {
+            label: 'history',
+            close: true
+          },
+          {
+            label: 'travel',
+            close: true
+          },
+        ]
+      }
+    },
+    computed: {
+      filteredPosts(){
+        return (this.posts.filter(item => item.text.toLowerCase().includes(this.inputValue.toLowerCase()) || item.title.toLowerCase().includes(this.inputValue.toLowerCase())) || this.posts)
+      },
+      filteredTags(){
+        return this.inputValue 
+          ? this.postsTags.filter(item => this.selectedCategories.includes(item.label.toLowerCase()))
+          : false
+      },
+      selectedCategories(){
+        const categories = []
+        this.filteredPosts.map(item => categories.push(item.category.toLowerCase()))
+        return  categories
       }
     }
 }
